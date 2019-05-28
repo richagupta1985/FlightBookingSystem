2b32b24f0654e3b87e8c91cd1bb434306348d487
richagupta1985 <richagupta1985@gmail.com>
2019-05-28 17:20:52 +0100

    Add files via upload

---
+++ FlightBookingSystem.Test/PrintSummaryTests.cs
@@ -0,0 +1,40 @@
+﻿using FlightBooking.Core;
+using FlightBooking.Core.BusinessLogic;
+using FlightBooking.Core.Model;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystem.Test
+{
+    [TestFixture]
+    public  class PrintSummaryTests
+    {
+        PrintSummary printSummary;
+        [SetUp]
+        public void Setup()
+        {
+            if (printSummary == null)
+            {
+                IEnumerable testData = FlightBookingTestData.TestCases;
+                List<Passenger> passengers = new List<Passenger>();
+                FlightBookingViewModel flightBookingViewModel = new FlightBookingViewModel();
+              
+                foreach (var data in testData)
+                {
+                    flightBookingViewModel.AnalysePassengerInput(((TestCaseData)data).Arguments[0].ToString());
+                }
+                printSummary = new PrintSummary(flightBookingViewModel.ScheduledFlight);
+            }
+        }
+        [Test]
+        public void CheckIfCanFlightProceedReturnsCorrectResult()
+        {
+            Assert.That(printSummary.CanFlightProceed(), Is.EqualTo(true));
+        }
+    }
+}
---
+++ FlightBookingSystem.Test/ScheduledFlightTests.cs
@@ -0,0 +1,46 @@
+﻿using FlightBooking.Core;
+using FlightBooking.Core.BusinessLogic;
+using FlightBooking.Core.Model;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FlightBookingSystem.Test
+{
+    [TestFixture]
+    public class ScheduledFlightTests
+    {
+        ScheduledFlight scheduledFlight;
+        [SetUp]
+        public void Setup()
+        {
+            if (scheduledFlight == null)
+            {
+                IEnumerable testData = FlightBookingTestData.TestCases;
+                List<Passenger> passengers = new List<Passenger>();
+                FlightBookingViewModel flightBookingViewModel = new FlightBookingViewModel();
+                scheduledFlight = flightBookingViewModel.ScheduledFlight;
+                foreach (var data in testData)
+                {
+                    flightBookingViewModel.AnalysePassengerInput(((TestCaseData)data).Arguments[0].ToString());
+                }
+            }
+        }
+
+        [Test]
+        public void CheckIfLoadFlightRouteDetailIsNotNull()
+        {
+            Assert.That(scheduledFlight.FlightRoute, Is.Not.Null);
+        }
+        [Test]
+        public void CheckIfPlaneDetailIsNotNull()
+        {
+            Assert.That(scheduledFlight.Aircraft, Is.Not.Null);
+        }
+        [Test]
+        public void CheckIfPassengersIsNotNull()
+        {
+            Assert.That(scheduledFlight.Passengers, Is.Not.Null);
+        }
+    }
+}
